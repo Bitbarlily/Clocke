1876e16b80195f1f6545dc2c56f5a261c6e8854a
Bitbarlily <lilysvanoe@gmail.com>
2024-12-29 14:30:16 +0100

    Refactored code
    Added back button

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,39 @@
 function updateTime() {
-  let bangkokElement = document.querySelector("#bangkok");
-  if (bangkokElement) {
-    let bangkokDateElement = bangkokElement.querySelector(".date");
-    let bangkokTimeElement = bangkokElement.querySelector(".time");
-    let bangkokTime = moment().tz(`Asia/Bangkok`);
-
-    bangkokDateElement.innerHTML = moment().format(`MMMM Do YYYY`);
-    bangkokTimeElement.innerHTML = bangkokTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
+  let cities = document.querySelector("#cities").querySelectorAll(".city");
+  if (cities && cities.length > 0) {
+    cities.forEach((cityElement) => {
+      updateCityElement(cityElement);
+    });
   }
+}
 
-  let osloElement = document.querySelector("#oslo");
-  if (osloElement) {
-    let osloDateElement = osloElement.querySelector(".date");
-    let osloTimeElement = osloElement.querySelector(".time");
-    let osloTime = moment().tz(`Europe/Oslo`);
+function updateCityElement(element) {
+  let dateElement = element.querySelector(".date");
+  let timeElement = element.querySelector(".time");
 
-    osloDateElement.innerHTML = moment().format(`MMMM Do YYYY`);
-    osloTimeElement.innerHTML = osloTime.format("h:mm:ss [<small>]A[</small>]");
-  }
+  let tzElement = element.querySelector(".tz-info-hidden");
+
+  let time = moment().tz(tzElement.innerHTML);
+
+  dateElement.innerHTML = moment().format(`MMMM Do YYYY`);
+  timeElement.innerHTML = time.format("h:mm:ss [<small>]A[</small>]");
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") {
+
+  if (cityTimeZone === "") {
+    onBack();
+  } else if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
+  <div class="tz-info-hidden">${cityTimeZone}</div>
     <div>
       <h2>${cityName}</h2>
               <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
@@ -40,11 +42,25 @@ function updateCity(event) {
                 "h:mm:ss"
               )} <small>${cityTime.format("A")}</small></div>
   </div>`;
+
+  let back = document.querySelector("#back");
+
+  if (back.classList.contains("hidden")) {
+    back.classList.remove("hidden");
+  }
+}
+
+function onBack() {
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.value = "";
+  window.location.reload();
 }
 
 let citiesSelectElement = document.querySelector("#city");
-
 citiesSelectElement.addEventListener("change", updateCity);
+citiesSelectElement.value = "";
+let back = document.querySelector("#back");
+back.addEventListener("click", onBack);
 
 updateTime();
 setInterval(updateTime, 1000);
